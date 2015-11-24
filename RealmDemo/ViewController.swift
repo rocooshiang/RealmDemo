8d98950288e63ecfee57e971777a00837690d769
rocooshiang <rocoo.shaing@gmail.com>
2015-11-24 21:14:55 +0800

    Initial Commit

---
+++ RealmDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by Geosat-RD01 on 2015/11/24.
+//  Copyright © 2015年 Geosat-RD01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
